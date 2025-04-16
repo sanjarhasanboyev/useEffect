8d99b0f8e21971cfc534c4f3699c4c41541e7c7d
Sanjar Hasanboyev <sanjarkhasanov11@gmail.com>
2025-04-16 16:28:33 +0500

    theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
+
   return (
     <div className="container mx-auto w-[80vw]">
       <BrowserRouter>
@@ -14,6 +16,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Card />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,10 @@
 import { NavLink } from "react-router-dom";
 import "../App.css";
-import { CiLight } from "react-icons/ci";
-import { useState } from "react";
-import { MdDarkMode } from "react-icons/md";
 
 function Navbar() {
-  const [theme, setTheme] = useState("light");
-  const handleTheme = () => {
-   setTheme(prev => prev === 'light' ? 'dark' : 'light');
-  };
 
   return (
-    <div className={`flex justify-between items-center py-2 ${theme === 'light' ? '' : 'bg-gray-600 text-amber-50'} `}>
+    <div className={`flex justify-between transition-colors duration-200 items-center py-2`}>
       <h1 className="text-2xl font-bold">Logo there</h1>
       <ul className="flex items-center gap-8">
         <li>
@@ -26,10 +19,9 @@ function Navbar() {
       </ul>
 
       <button
-        onClick={handleTheme}
         className="border rounded px-7 py-1 cursor-pointer"
       >
-        {theme === "light" ? <CiLight /> : <MdDarkMode />}
+        Dark mode
       </button>
     </div>
   );
